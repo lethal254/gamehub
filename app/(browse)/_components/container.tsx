225f42eab64baaa936eea2707a0dde0009d07258
lethal254 <benardogutu65@gmail.com>
2024-01-02 00:47:03 +0300

    Implemented side and nav bars

---
+++ app/(browse)/_components/container.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { useSidebar } from "@/store/use-sidebar"
+import { useEffect } from "react"
+import { useMediaQuery } from "usehooks-ts"
+
+interface ContainerProps {
+  children: React.ReactNode
+}
+
+const Container = ({ children }: ContainerProps) => {
+  const { collapsed, onCollapse, onExpand } = useSidebar((state) => state)
+  const matches = useMediaQuery("(max-width: 1024px)")
+
+  useEffect(() => {
+    if (matches) {
+      onCollapse()
+    } else {
+      onExpand()
+    }
+  }, [matches])
+  return (
+    <div
+      className={cn("flex-1", collapsed ? "ml-[70px]" : "ml-[70px] lg:ml-60")}>
+      {children}
+    </div>
+  )
+}
+
+export default Container
